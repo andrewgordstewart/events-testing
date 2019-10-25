40fb72fce296c3861a2bad746de520263b4af785
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-10-25 14:33:14 -0230

    Add test where register is called multiple times

---
+++ test/multipleRegistrations.test.js
@@ -0,0 +1,34 @@
+const ethers = require('ethers');
+const RegistrationArtifact = require('../build/Registration.json');
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+const signer = provider.getSigner(0);
+
+jest.setTimeout(10000);
+
+it('Sometimes times out when two registrations occur', async () => {
+    let contract = await new ethers.ContractFactory(
+        RegistrationArtifact.abi,
+        RegistrationArtifact.bytecode,
+        signer
+    ).deploy();
+
+    const filter = contract.filters.Registered(1);
+    
+    let registration = new Promise((resolve, reject) => {
+        contract.on(filter, (idx, event) => {
+            event.removeListener();
+            
+            resolve({ idx: idx });
+        });
+
+        setTimeout(() => {
+            reject(new Error('timeout'));
+        }, 8000)
+    });
+    
+    await contract.register(1);
+    await contract.register(2);
+
+    const event = await registration;
+    expect(event.idx._hex).toBe('0x01')
+})
